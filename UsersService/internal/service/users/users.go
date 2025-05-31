eba241bbf451cefa930f6837a941a5f358181c03
Artem Isachenkov <artemyt237@gmail.com.com>
2025-05-31 11:47:45 +0300

    service done

--- UsersService/internal/service/users/users.go
+++ UsersService/internal/service/users/users.go
@@ -2,8 +2,13 @@ package usersservice
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"log/slog"
 	"usersservice/internal/domain/models"
+	serviceerror "usersservice/internal/service"
+	storageerror "usersservice/internal/storage"
+	"usersservice/pkg/lib/logger/sl"
 
 	"github.com/google/uuid"
 )
@@ -30,25 +35,130 @@ func New(log *slog.Logger, storage IUsersStorage) *UsersService {
 
 // GetUsers implements grpcapp.IUsersService.
 func (u *UsersService) GetUsers(ctx context.Context) ([]models.User, error) {
-	panic("unimplemented")
+	const op = "service.users.GetUsers"
+	log := u.log.With(
+		"op", op,
+	)
+
+	select {
+	case <-ctx.Done():
+		return nil, fmt.Errorf("%s: %w", op, ctx.Err())
+	default:
+	}
+
+	users, err := u.storage.GetUsers(ctx)
+	if err != nil {
+		log.Error("Error fetching users", sl.Err(err))
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return users, nil
 }
 
 // GetUserById implements grpcapp.IUsersService.
 func (u *UsersService) GetUserById(ctx context.Context, uid uuid.UUID) (models.User, error) {
-	panic("unimplemented")
+	const op = "service.users.GetUserById"
+	log := u.log.With(
+		"op", op,
+	)
+
+	select {
+	case <-ctx.Done():
+		return models.User{}, fmt.Errorf("%s: %w", op, ctx.Err())
+	default:
+	}
+
+	user, err := u.storage.GetUserById(ctx, uid)
+	if err != nil {
+		if errors.Is(err, storageerror.ErrNotFound) {
+			log.Warn("User not found", sl.Err(serviceerror.ErrNotFound))
+			return models.User{}, fmt.Errorf("%s: %w", op, serviceerror.ErrNotFound)
+		}
+
+		log.Error("Error fetching user by id", sl.Err(err))
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return user, nil
 }
 
 // Insert implements grpcapp.IUsersService.
-func (u *UsersService) Insert(ctx context.Context, user models.User) (models.User, error) {
-	panic("unimplemented")
+func (u *UsersService) Insert(ctx context.Context, userForInsert models.User) (models.User, error) {
+	const op = "service.users.Insert"
+	log := u.log.With(
+		"op", op,
+	)
+
+	select {
+	case <-ctx.Done():
+		return models.User{}, fmt.Errorf("%s: %w", op, ctx.Err())
+	default:
+	}
+
+	insertedUser, err := u.storage.Insert(ctx, userForInsert)
+	if err != nil {
+		if errors.Is(err, storageerror.ErrAlreadyExists) {
+			log.Warn("User already exists", sl.Err(storageerror.ErrAlreadyExists))
+			return models.User{}, fmt.Errorf("%s: %w", op, serviceerror.ErrAlreadyExists)
+		}
+
+		log.Error("Cannot insert user", sl.Err(err))
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return insertedUser, nil
 }
 
 // Update implements grpcapp.IUsersService.
-func (u *UsersService) Update(ctx context.Context, uid uuid.UUID, user models.User) (models.User, error) {
-	panic("unimplemented")
+func (u *UsersService) Update(ctx context.Context, uid uuid.UUID, userForUpdate models.User) (models.User, error) {
+	const op = "service.users.Update"
+	log := u.log.With(
+		"op", op,
+	)
+
+	select {
+	case <-ctx.Done():
+		return models.User{}, fmt.Errorf("%s: %w", op, ctx.Err())
+	default:
+	}
+
+	updatedUser, err := u.storage.Update(ctx, uid, userForUpdate)
+	if err != nil {
+		if errors.Is(err, storageerror.ErrNotFound) {
+			log.Warn("User not found", sl.Err(storageerror.ErrNotFound))
+			return models.User{}, fmt.Errorf("%s: %w", op, serviceerror.ErrNotFound)
+		}
+
+		log.Error("Error updating user", sl.Err(err))
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return updatedUser, nil
 }
 
 // Delete implements grpcapp.IUsersService.
 func (u *UsersService) Delete(ctx context.Context, uid uuid.UUID) (models.User, error) {
-	panic("unimplemented")
+	const op = "service.users.Delete"
+	log := u.log.With(
+		"op", op,
+	)
+
+	select {
+	case <-ctx.Done():
+		return models.User{}, fmt.Errorf("%s: %w", op, ctx.Err())
+	default:
+	}
+
+	deletedUser, err := u.storage.Delete(ctx, uid)
+	if err != nil {
+		if errors.Is(err, storageerror.ErrNotFound) {
+			log.Warn("User not found", sl.Err(storageerror.ErrNotFound))
+			return models.User{}, fmt.Errorf("%s: %w", op, serviceerror.ErrNotFound)
+		}
+
+		log.Error("Error deleting user", sl.Err(err))
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return deletedUser, nil
 }
