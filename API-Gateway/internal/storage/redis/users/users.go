4830e616969e2a5daa2ebc2878f08baf403791f1
Artem Isachenkov <artemyt237@gmail.com.com>
2025-06-11 10:03:17 +0300

    done redis storage

---
+++ API-Gateway/internal/storage/redis/users/users.go
@@ -0,0 +1,137 @@
+package userscashstorage
+
+import (
+	"api-gateway/internal/domain/models"
+	"api-gateway/pkg/lib/logger/sl"
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+)
+
+var ExpirationTime = 10
+
+type UsersCashStorage struct {
+	log *slog.Logger
+	rds *redis.Client
+}
+
+func New(log *slog.Logger, connStr string) *UsersCashStorage {
+	rds := redis.NewClient(&redis.Options{
+		Addr:     connStr,
+		Password: "",
+		DB:       0,
+	})
+
+	if err := rds.Ping(context.TODO()).Err(); err != nil {
+		panic(err)
+	}
+
+	return &UsersCashStorage{
+		log: log,
+		rds: rds,
+	}
+}
+
+// Get implements userscashservice.UsersCashStorage.
+func (u *UsersCashStorage) Get(ctx context.Context, id uuid.UUID) (models.User, error) {
+	const op = "storage.redis.users.Get"
+	log := u.log.With(
+		"op", op,
+	)
+
+	select {
+	case <-ctx.Done():
+		return models.User{}, fmt.Errorf("%s: %w", op, ctx.Err())
+	default:
+	}
+
+	userFromRedis, err := u.rds.HGetAll(
+		ctx,
+		fmt.Sprintf("user:%s", id.String()),
+	).Result()
+	if err != nil {
+		log.Info("User not found in redis", sl.Err(err))
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	userForReturn := mapToUser(userFromRedis)
+	return userForReturn, nil
+}
+
+// Set implements userscashservice.UsersCashStorage.
+func (u *UsersCashStorage) Set(ctx context.Context, user models.User) error {
+	const op = "storage.redis.users.Set"
+	log := u.log.With(
+		"op", op,
+	)
+
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("%s: %w", op, ctx.Err())
+	default:
+	}
+
+	_, err := u.rds.HSet(
+		ctx,
+		fmt.Sprintf("user:%s", user.Id.String()),
+		map[string]string{
+			"login":    user.Login,
+			"password": string(user.Password),
+			"role":     user.Role,
+		},
+	).Result()
+	if err != nil {
+		log.Warn("Cannot insert user to redis", sl.Err(err))
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = u.rds.Expire(
+		ctx,
+		fmt.Sprintf("user:%s", user.Id.String()),
+		time.Duration(ExpirationTime),
+	).Result()
+	if err != nil {
+		log.Warn("Cannot set expiration time for user"+user.Id.String(), sl.Err(err))
+	}
+
+	return nil
+}
+
+// Del implements userscashservice.UsersCashStorage.
+func (u *UsersCashStorage) Del(ctx context.Context, id uuid.UUID) error {
+	const op = "storage.redis.users.Del"
+	log := u.log.With(
+		"op", op,
+	)
+
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("%s: %w", op, ctx.Err())
+	default:
+	}
+
+	_, err := u.rds.HDel(
+		ctx,
+		fmt.Sprintf("user:%s", id.String()),
+	).Result()
+	if err != nil {
+		log.Warn("Cannot delete user:"+id.String()+" from cash", sl.Err(err))
+	}
+
+	return nil
+
+}
+
+func mapToUser(mappedUser map[string]string) models.User {
+	id, _ := uuid.Parse(mappedUser["id"])
+	return models.User{
+		Id:       id,
+		Login:    mappedUser["login"],
+		Password: []byte(mappedUser["password"]),
+		Role:     mappedUser["role"],
+	}
+}
