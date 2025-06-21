448d1d6a6caa0a88c32e426efd48dbbef41773b2
Artem Isachenkov <artemyt237@gmail.com>
2025-06-21 16:50:55 +0300

    done login service

--- Auth/internal/service/auth/auth.go
+++ Auth/internal/service/auth/auth.go
@@ -2,7 +2,11 @@ package authservice
 
 import (
 	"auth/internal/domain/models"
+	"auth/pkg/lib/logger/sl"
+	"bytes"
 	"context"
+	"errors"
+	"fmt"
 	"log/slog"
 
 	"github.com/google/uuid"
@@ -30,7 +34,37 @@ func New(log *slog.Logger, storage IUsersStorage) *AuthService {
 
 // Login implements grpcapp.IAuthService.
 func (a *AuthService) Login(ctx context.Context, login string, password []byte) (string, string, error) {
-	panic("unimplemented")
+	const op = "service.auth.Login"
+	log := a.log.With(
+		"op", op,
+	)
+
+	select {
+	case <-ctx.Done():
+		return "", "", fmt.Errorf("%s: %w", op, ctx.Err())
+	default:
+	}
+
+	users, err := a.storage.GetUsers(ctx)
+	if err != nil {
+		log.Error("Failed to get user", sl.Err(err))
+		return "", "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	var loggedUser = models.User{Login: "undef"}
+	for _, user := range users {
+		if user.Login == login && bytes.Equal(user.Password, password) {
+			loggedUser = user
+		}
+	}
+	if loggedUser.Login == "undef" {
+		log.Error("User doesn't exists", sl.Err(err))
+		return "", "", fmt.Errorf("%s: %w", op, errors.New("user doesn't exists"))
+	}
+
+	// генерация токенов
+	accessToken, refreshToken, err := "", "", nil
+	return accessToken, refreshToken, nil
 }
 
 // Register implements grpcapp.IAuthService.
